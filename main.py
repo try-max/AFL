89d9e36b0e2db646fa6fa9e281178e8a52d5cf63
try-max <qijiale_1128@163.com>
2022-03-12 18:35:46 +0800

    0312日常提交

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 
-from flask import Flask
+from flask import Flask,render_template
 import config
 
 app = Flask(__name__)
@@ -12,9 +12,15 @@ def helloword():
     # return  "hello,world2! 你好"
     return {"username":"知乎1"}
 
+@app.route('/about')
+def about():
+    return render_template("about.html")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     app.run()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+# redirect(url_for("index")) 重定向，指定到url_for指定的指定方法的路径
